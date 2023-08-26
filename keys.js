9527f9434dcc5b659a77378c222b50ec2b8e2a56
Wasaanyi <jwasaanyi@gmail.com>
2023-08-26 23:08:27 +0300

    updating keys file

--- keys.js
+++
@@ -1,8 +0,0 @@
-// API Key - https://developer.google.com/custom-search/v1/using_rest
-export const API_KEY = 'AIzaSyAxZPVoBtU-YimmAD7z4xB8TIF_dD_ZN7c'
-
-
-// Context Key - https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = 'd61ac511736e841c4'
-
-//https://www.googleapis.com/customsearch/v1?key=AIzaSyAxZPVoBtU-YimmAD7z4xB8TIF_dD_ZN7c&cx=d61ac511736e841c4&q=james
